26955cd349df930f2ca6501e96c11f4d03baaafe
Ronio <Ronio.hu@gmail.com>
2016-10-28 02:06:03 +0800

    公告列表和详情页

---
+++ src/views/notice/detail.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-article 
+        :title="title" 
+        :meta="meta" 
+        :content="content">
+    </v-article>
+</template>
+
+<script>
+import vArticle from 'components/Article'
+export default {
+  
+  data () {
+    return {
+        title: '七部委启动第三批电子商务示范城市申报工作',
+        meta: '8-11 09:00',
+        content: '很多内容离开就是来的快放假了空间路上看到减肥路上看到减肥离开就是来的快放假老师打开减肥离开就是来的快放假'
+    }
+  },
+  components: {
+    vArticle
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/views/notice/list.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="m-list grid" v-for="item in 4" @click="toDetail('/notice/detail')">
+        <h1 class="title">七部委启动第三批电子商务示范城市申报工作</h1>
+        <p class="desc">中国证券网讯（记者 郭晓萍）记者31日从发改委网站获悉，国家发展改革委、商务部</p>
+        <div class="time">8-11 09:00</div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  },
+  methods: {
+    toDetail(link) {
+        this.$router.go(link);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "~src/styles/mixin.less";
+
+.m-list {
+    display: flex;
+    flex-flow: column;
+    padding: .4rem 0;
+    border-bottom: 1px solid @bd-color;
+    .title {
+        .font-size(@fs30);
+        line-height: 2em;
+    }
+    .desc {
+        line-height: 1.5em;
+        font-size: @fs28;
+        color: @c-666666;
+        flex:　1;
+        &:after {
+            content: '...';
+        }
+    }
+    .time {
+        text-align: right;
+        color: @c-999999;
+    }
+}
+</style>
